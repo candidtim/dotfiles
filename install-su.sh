2a10a6232d0ed5d61c855936d55775b12132d105
Timur Rubeko <timur@rubeko.com>
2022-03-17 23:52:33 +0100

    Add ctags

--- install-su.sh
+++ install-su.sh
@@ -3,6 +3,6 @@
 # install some packages
 apt update
 apt -y install curl python3-pip vim vim-gtk git zsh silversearcher-ag \
-  npm feh build-essential cmake tmux gnome-tweaks
+  npm feh build-essential cmake tmux gnome-tweaks exuberant-ctags
 pip3 install virtualenvwrapper
 npm install -g diff-so-fancy
